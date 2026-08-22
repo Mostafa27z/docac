7d8da7f99cdbe1088912c2dba0dad0741bef337f
mostafaKeen <mostafa@keenenter.com>
2026-08-22 17:40:40 +0300

    first edits 2

--- resources/views/components/form-select.blade.php
+++ resources/views/components/form-select.blade.php
@@ -4,7 +4,7 @@
     <label for="{{ $name }}" class="block text-sm font-semibold text-[#4A5568] mb-1.5">{{ $label }}</label>
     <select
         name="{{ $name }}"
-        id="{{ $name }}"
+        id="{{ $attributes->get('id', $name) }}"
         {{ $required ? 'required' : '' }}
         {{ $attributes->merge(['class' => 'w-full bg-[#F8F9FA] border border-[#E2E8F0] rounded-xl px-4 py-2.5 text-[#1A202C] text-sm focus:outline-none focus:ring-2 focus:ring-[#0047AB]/20 focus:border-[#0047AB] transition-all duration-200']) }}
     >
